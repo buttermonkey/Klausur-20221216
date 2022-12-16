f5b989a3e7cacfa12b3768e8440af4d27c09f523
Markus Stoff <s52005@edu.campus02.at>
2022-12-16 14:16:48 +0100

    Beispiel 2 - Recursion

---
+++ src/org/campus02/recursion/RecursiveMultiplication.java
@@ -0,0 +1,21 @@
+package org.campus02.recursion;
+
+public class RecursiveMultiplication {
+	public static void main(String[] args) {
+		final int NUMBER = 5;
+		int[] testData = new int[]{ 0, 1, 2, 5, 40, -10, -1, -2 };
+		for (int factor : testData) {
+			System.out.println(NUMBER + " x " + factor + " = " + multiplicate(NUMBER, factor));
+			System.out.println(factor + " x " + NUMBER + " = " + multiplicate(factor, NUMBER));
+		}
+	}
+	public static int multiplicate(int a, int b) {
+		if (b == 0)
+			return 0;
+		if (b == 1)
+			return a;
+		if (b < 0)
+			return multiplicate(a, b * (-1)) * (-1);
+		return a + multiplicate(a, b - 1);
+	}
+}
